6fcf2e745cb21281454f6fdcd36a8e95b2165316
Alexander Shershnev <shershnev.av@gmail.com>
2014-07-04 12:09:11 +0400

    Empty local settings

--- server/config/local-settings.js
+++ server/config/local-settings.js
@@ -2,11 +2,7 @@
 
 var
     LOCAL_SETTINGS = {
-        secret: 'techreuhrgejrvnsjeriuverviebriberijdbc42634',
-        port: 1337,
-        database: {
-            url: 'mongodb://tech:tech_3777489@ds045089.mongolab.com:45089/tech'
-        }
+        /* Copy from settings.js and change for yourself */
     };
 
 module.exports = LOCAL_SETTINGS;
