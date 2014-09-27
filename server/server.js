bc4e3cb250cc8522162c6e40340aa4e076dacec6
Shershnev Alexandr <shershnev.av@gmail.com>
2014-09-27 12:05:59 +0400

    Fix server after updating packages

--- server/server.js
+++ server/server.js
@@ -1,49 +1,54 @@
 'use strict';
 
 var
-    CLIENT_SETTINGS = require('./config/client-settings'),
-    SETTINGS = require('./config/settings'),
+  CLIENT_SETTINGS = require('./config/client-settings'),
+  SETTINGS = require('./config/settings'),
 
-    express = require('express'),
-    mongoose = require('mongoose'),
+  express = require('express'),
+  mongoose = require('mongoose'),
 
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    flash = require('connect-flash'),
-    passport = require('./auth/passport'),
-    compression = require('compression'),
-    MongoStore = require('connect-mongo')(session),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  flash = require('connect-flash'),
+  passport = require('./auth/passport'),
+  compression = require('compression'),
+  MongoStore = require('connect-mongo')(session),
 
-    router = require('./router'),
-    authRouter = require('./auth/auth-router'),
-    apiRouter = require('./api/router'),
+  router = require('./router'),
+  authRouter = require('./auth/auth-router'),
+  apiRouter = require('./api/router'),
 
-    app = express();
+  app = express();
 
 mongoose
-    .connect(SETTINGS.database.url);
+  .connect(SETTINGS.database.url);
 
 app
-    .use(express.static(CLIENT_SETTINGS.STATIC_FILES_PATH));
+  .use(express.static(CLIENT_SETTINGS.STATIC_FILES_PATH));
 
 app
-    .use(compression())
-    .use(bodyParser())
-    .use(session({
-            secret: SETTINGS.secret,
-            cookie: { maxAge: 31536000000 },
-            store: new MongoStore({ url: SETTINGS.database.url })
-        }))
-    .use(passport.initialize())
-    .use(passport.session())
-    .use(flash());
+  .use(compression())
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+  extended: true
+  }))
+  .use(session({
+    secret: SETTINGS.secret,
+    cookie: { maxAge: 31536000000 },
+    store: new MongoStore({ url: SETTINGS.database.url }),
+    resave: true,
+    saveUninitialized: true
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(flash());
 
 app
-    .use('/', authRouter)
-    .use('/api', apiRouter)
-    .use('*', router);
+  .use('/', authRouter)
+  .use('/api', apiRouter)
+  .use('*', router);
 
 app
-    .listen(SETTINGS.port, function () {
-        console.log('Express server listening on port ' + SETTINGS.port);
-    });
+  .listen(SETTINGS.port, function () {
+    console.log('Express server listening on port ' + SETTINGS.port);
+  });
